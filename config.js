446741b1bba380a90279a1f4b41de102d4f880a4
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-01 10:05:49 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-   ['51906168999', 'Maricielo', true],
+   ['', 'Jota', true],
    ['51927238856', 'Cristian Escobar', true],
    ['51990841568', 'Moderador', false],
    ['51965911060', 'Moderador', false]
@@ -22,28 +22,28 @@ global.mods = ['51990841568', '51965911060', '51906168999']
 global.prems = ['51965911060', '51906168999']
 
 
-global.packname = 'Morchiyara Bot'
-global.botname = 'Morchi Bot - MD'
-global.wm = '⏤͟͟͞͞⋆⬪࣪ꥈ🐱 ׄ ꒱ Morchi ୭'
+global.packname = 'Jota Bot'
+global.botname = 'Jota Bot - MD'
+global.wm = '⏤͟͟͞͞⋆⬪࣪ꥈ👺 ׄ ꒱ Jota ୭'
 global.author = 'Nᴏᴠʌ Ȿᴘʌʀᴋ Cᴏʍʍᴜɴɪᴛʏ'
 global.dev = 'Cʀᴇᴅɪᴛs ᴛᴏ Nᴏᴠʌ Ȿᴘʌʀᴋ Cᴏʍʍᴜɴɪᴛʏ'
 global.errorm = 'Error: ${error.message}'
-global.nombrebot = 'MorchiBot - MD'
-global.textbot = `🐱 𝗠𝗢𝗥𝗖𝗛𝗜 𝗕𝗢𝗧 🐱`
+global.nombrebot = 'JotaBot - MD'
+global.textbot = `JOTA - BOT 🫧`
 global.vs = '1.0.0'
 
 
 global.catalogo = fs.readFileSync('./media/catalogo.jpg')
 
 
-global.repobot = 'https://github.com/CrxstianEscobar/MorchiBot'
+global.repobot = 'https://github.com/NovaSpark-Community/JotaBot'
 global.grupo = 'https://chat.whatsapp.com/FCS6htvAmlT7nq006lxU4I'
 global.comu = 'https://chat.whatsapp.com/Kn1pPVAO08pFRo7qJnKuh6'
 global.channel = 'https://whatsapp.com/channel/0029VauTE8AHltY1muYir31n'
-global.insta = 'https://www.instagram.com/morchi_yara'
+global.insta = 'https://www.instagram.com/dev.criss_vx'
 
 
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "543876577197-120363317332020195@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'MOONFORCE 乂 TEAM', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "543876577197-120363317332020195@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'SUNFLARE 乂 TEAM', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 
 
 global.cheerio = cheerio
