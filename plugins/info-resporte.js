4af5ac6ee60ed05015031168a782cbae55669d42
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-02 17:30:18 -0500

    Actualizar info-resporte.js

--- plugins/info-resporte.js
+++ plugins/info-resporte.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     try {
         text = text.trim();
-        if (!text) return m.reply('*😸 Ingrese el error que desea reportar.*');
-        if (text.length < 10) return m.reply('*😾 Especifique bien el error, mínimo 10 caracteres.*');
+        if (!text) return m.reply(`*${emojis} Ingrese el error que desea reportar.*`);
+        if (text.length < 10) return m.reply(`*${emojis} Especifique bien el error, mínimo 10 caracteres.*`);
         if (text.length > 1000) return m.reply('*⚠️  Máximo 1000 caracteres para enviar el error.*');
 
         const sender = m.sender.split('@')[0];
