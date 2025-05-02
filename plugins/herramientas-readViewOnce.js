5cba2951eeb81b698eb2f0facf031635017d2e86
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-02 17:42:24 -0500

    Actualizar herramientas-readViewOnce.js

--- plugins/herramientas-readViewOnce.js
+++ plugins/herramientas-readViewOnce.js
@@ -3,8 +3,8 @@
 let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
 let handler = async (m, { conn }) => {
-if (!m.quoted) return conn.reply(m.chat, `*🐈  Responde a una imagen ViewOnce (ver solo vez)*.`, m)
-if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `*🐈 Responde a una imagen ViewOnce (ver solo una vez)*`, m)
+if (!m.quoted) return conn.reply(m.chat, `*${emojis} Responde a una imagen ViewOnce (ver solo vez)*.`, m)
+if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `*${emojis} Responde a una imagen ViewOnce (ver solo una vez)*`, m)
 let buffer = await m.quoted.download(false);
 if (/videoMessage/.test(m.quoted.mtype)) {
 return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m)
