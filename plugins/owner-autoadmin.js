a6d057c2a31a5aa6b30a48957c09e002d5829479
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-02 17:16:36 -0500

    Actualizar owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,9 +1,9 @@
 const handler = async (m, {conn, isAdmin, groupMetadata }) => {
-  if (isAdmin) return m.reply('*🤍 Ya eres administrador del grupo.*');
+  if (isAdmin) return m.reply(`*${emojis} Ya eres administrador del grupo.*`);
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
   await m.react(done)
-   m.reply('*🌿 ¡Ahora eres administrador del grupo! :3*');
+   m.reply(`*${emojis} ¡Mi creador ahora es admin del grupo!*`);
     let nn = conn.getName(m.sender);
      //conn.reply('543876577197@s.whatsapp.net', `☕ *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, );
   } catch {
