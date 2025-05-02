ad010b2b924d39cf236444f35d031540da97181b
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-02 16:34:04 -0500

    Actualizar sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,6 +1,6 @@
 import { addExif } from '../lib/sticker.js'
 let handler = async (m, { conn, text }) => {
-  if (!m.quoted) return m.reply(`*${emojis} Responde al sticker.*`, m, rcanal)
+  if (!m.quoted) return m.reply(`*${emojis} Responde al sticker.*`)
   let stiker = false
   try {
     let [packname, ...author] = text.split('|')
@@ -8,14 +8,14 @@ let handler = async (m, { conn, text }) => {
     let mime = m.quoted.mimetype || ''
     if (!/webp/.test(mime)) return m.reply(`*${emojis} Responde al sticker.*`)
     let img = await m.quoted.download()
-    if (!img) return m.reply(`*${emojis} Responde al sticker.*`, m, rcanal)
+    if (!img) return m.reply(`*${emojis} Responde al sticker.*`)
     stiker = await addExif(img, packname || '', author || '')
   } catch (e) {
     console.error(e)
     if (Buffer.isBuffer(e)) stiker = e
   } finally {
     if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m)
-    else return m.reply(`*${emojis} Responde al sticker.*`, m, rcanal)
+    else return m.reply(`*${emojis} Responde al sticker.*`)
   }
 }
 handler.help = ['wm']
