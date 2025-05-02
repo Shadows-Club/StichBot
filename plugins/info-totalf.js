aac04489f33cb572a271f87a23fb9a30de46fd2d
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-02 17:26:32 -0500

    Actualizar info-totalf.js

--- plugins/info-totalf.js
+++ plugins/info-totalf.js
@@ -14,7 +14,7 @@ const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
   }, 0);
 
   conn.sendMessage(m.chat, {
-text: `*🐼 ¡El poder está en tus manos!*\n*Bienvenido ${taguser}*\n\n*Este bot cuenta con \`${totalf}\` comandos disponibles para ti. ¿Tienes alguna sugerencia para mejorar nuestra experiencia? ¡Usa el comando \`.sugerir\` para saber que idea tienes!*\n\n> © Jota Bot - MD`,
+text: `*🤍 ¡El poder está en tus manos!*\n*Bienvenido ${taguser}*\n\n*Este bot cuenta con \`${totalf}\` comandos disponibles para ti. ¿Tienes alguna sugerencia para mejorar nuestra experiencia? ¡Usa el comando \`.sugerir\` para saber que idea tienes!*\n\n> © ${botname}`,
 mentions: [m.sender]
 }, { quoted: fkontak });
 };
