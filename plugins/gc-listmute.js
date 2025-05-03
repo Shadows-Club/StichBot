262278cf61b7b95f4260cf3835b1842fc7149d4f
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-03 14:23:48 -0500

    Actualizar plugins

---
+++ plugins/gc-listmute.js
@@ -0,0 +1,23 @@
+let mutedUsers = new Set();
+
+let handler = async (m, { conn, usedPrefix, command, isAdmin, isBotAdmin }) => {
+    if (!isBotAdmin) return conn.reply(m.chat, '🍭 El bot necesita ser administrador.', m);
+    if (!isAdmin) return conn.reply(m.chat, '🍭 Solo los administradores pueden usar este comando.', m);
+
+    if (command === "listmute") { // Nuevo comando para listar los muteados
+        if (mutedUsers.size === 0) {
+            return conn.reply(m.chat, '🍭 No hay usuarios muteados en este grupo.', m);
+        }
+        let muteList = [...mutedUsers].map(user => `@${user.split('@')[0]}`).join('\n');
+        conn.reply(m.chat, `🍭 *Usuarios muteados en este grupo:*\n${muteList}`, m, { mentions: [...mutedUsers] });
+    }
+};
+
+handler.help = ['listmute'];
+handler.tags = ['grupo'];
+handler.command = /^(listmute)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+
+export default handler;
