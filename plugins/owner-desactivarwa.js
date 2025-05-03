85e489be0c310b4cbe97f92ecd778625ff0969a6
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-02 21:11:56 -0500

    Actualizar owner-desactivarwa.js

--- plugins/owner-desactivarwa.js
+++ plugins/owner-desactivarwa.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 let handler = async (m, { conn, usedPrefix, command }) => {
     const maria = 'https://files.catbox.moe/6911v7.jpg';
+
     const buttons = [
         {
             buttonId: `.owner`,
@@ -15,6 +16,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
         {
             image: { url: maria },
             caption: "*JOTA BOT*",
+            footer: dev,
             buttons: buttons,
             viewOnce: true
         },
@@ -27,3 +29,31 @@ handler.tags = ["info"];
 handler.command = /^(precios)$/i;
 
 export default handler;
+/*
+
+
+        await conn.sendMessage(m.chat, {
+            image: thumbnail,
+            caption: messageText,
+            footer: 'Code by Cristian Escobar',
+            contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 999,
+                isForwarded: true
+            },
+            buttons: [
+                {
+                    buttonId: `${usedPrefix}ytmp3 ${video.url}`,
+                    buttonText: { displayText: 'Audio' },
+                    type: 1,
+                },
+                {
+                    buttonId: `${usedPrefix}ytmp4 ${video.url}`,
+                    buttonText: { displayText: 'Vídeo' },
+                    type: 1,
+                }
+            ],
+            headerType: 1,
+            viewOnce: true
+        }, { quoted: m });
+*/
