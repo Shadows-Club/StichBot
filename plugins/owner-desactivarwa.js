30c479d5267ef0c39fc391756af0394b8383f1af
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-03 08:21:43 -0500

    Actualizar owner-desactivarwa.js

--- plugins/owner-desactivarwa.js
+++ plugins/owner-desactivarwa.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+/*import axios from "axios";
 
 let handler = async (m, { conn, usedPrefix, command }) => {
     const comprar = 'https://files.catbox.moe/6911v7.jpg';
@@ -30,7 +30,7 @@ handler.command = /^(precios)$/i;
 
 export default handler;
 
-/*
+
 
 
         await conn.sendMessage(m.chat, {
@@ -58,3 +58,63 @@ export default handler;
             viewOnce: true
         }, { quoted: m });
 */
+
+import axios from "axios";
+
+let handler = async (m, { conn, command }) => {
+    const comprar = 'https://files.catbox.moe/6911v7.jpg';
+
+    // Reacción según comando
+    await m.react(command === 'vendedor' ? '🤍' : '🛒');
+
+    if (command === 'vendedor') {
+        let username = await conn.getName(m.sender);
+
+        // Mensaje explicativo
+        await conn.sendMessage(m.chat, {
+            text: `👤 *Hola ${username}*\n\nAquí tienes el contacto del creador para adquirir el bot *JOTA BOT*.\nPuedes escribirle para más detalles o contratar tu propio bot personalizado.`
+        }, { quoted: m });
+
+        // Enviar contacto
+        let list = [{
+            displayName: "Cristian Escobar",
+            vcard: `BEGIN:VCARD\nVERSION:3.0\nFN: Cristian Escobar\nitem1.TEL;waid=51927238856:51927238856\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET: cristianescobar.vx@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/dev.criss_vx\nitem3.X-ABLabel:Instagram\nitem4.ADR:;; Perú;;;;\nitem4.X-ABLabel:Región\nEND:VCARD`,
+        }];
+
+        await conn.sendMessage(m.chat, {
+            contacts: {
+                displayName: `${list.length} Contacto`,
+                contacts: list
+            }
+        }, { quoted: m });
+
+        return;
+    }
+
+    // Enviar imagen con botón si el comando es .precios
+    const buttons = [
+        {
+            buttonId: `.vendedor`,
+            buttonText: { displayText: "Comprar 🛍️" },
+            type: 1
+        }
+    ];
+
+    await conn.sendMessage(
+        m.chat,
+        {
+            image: { url: comprar },
+            caption: "*JOTA BOT*",
+            footer: "By Cristian Escobar",
+            buttons: buttons,
+            viewOnce: true
+        },
+        { quoted: m }
+    );
+};
+
+handler.help = ["precios", "vendedor"];
+handler.tags = ["info"];
+handler.command = /^(precios|vendedor)$/i;
+
+export default handler;
