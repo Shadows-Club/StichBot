02c32e368eac147d87f7d368771a07178d3741c3
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-03 19:10:29 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,22 +3,22 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://qu.ax/kgzBh.jpg')
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => '')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
 
-  if (chat.bienvenida && m.messageStubType == 27) {
+  if (chat.welcome && m.messageStubType == 27) {
     let bienvenida = `┌─★ *${botname}* \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
 
-await conn.sendLuffy(m.chat, botname, textbot, bienvenida, img, img, canal, estilo)
+await conn.sendLuffy(m.chat, botname, textbot, bienvenida, img, img, canal, fkontak)
   }
 
-  if (chat.bienvenida && m.messageStubType == 28) {
+  if (chat.welcome && m.messageStubType == 28) {
     let bye = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
 await conn.sendLuffy(m.chat, botname, textbot, bye, img, img, canal, fkontak)
   }
 
-  if (chat.bienvenida && m.messageStubType == 32) {
+  if (chat.welcome && m.messageStubType == 32) {
     let kick = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
 await conn.sendLuffy(m.chat, botname, textbot, kick, img, img, canal, fkontak)
 }}
