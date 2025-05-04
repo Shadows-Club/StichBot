9e96d4754185ad62012e42758bb04c3fdd52fe06
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-04 09:33:19 -0500

    Actualizar buscador-tiktoksearch.js

--- plugins/buscador-tiktoksearch.js
+++ plugins/buscador-tiktoksearch.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const { proto, generateWAMessageFromContent, generateWAMessageContent } = (await import("@whiskeysockets/baileys")).default;
 
 let handler = async (message, { conn, text, usedPrefix, command }) => {
-  if (!text) return conn.reply(message.chat, '*🐈 Ingrese el texto que desee buscar en TikTok*', message);
+  if (!text) return conn.reply(message.chat, `*${emojis} Ingrese el texto que desee buscar en TikTok*`, message);
 
   // Función para crear el mensaje de video
   async function createVideoMessage(url) {
