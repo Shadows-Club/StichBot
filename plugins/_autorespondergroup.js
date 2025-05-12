59371a6c7fb250037c56ce2128ec6bfd2956a6b8
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-12 17:08:03 -0500

    Actualizar _autorespondergroup.js

--- plugins/_autorespondergroup.js
+++ plugins/_autorespondergroup.js
@@ -1,13 +1,15 @@
 import fs from 'fs';
+
 const handler = (m) => m;
-handler.all = async function(m) {
 
-const chat = global.db.data.chats[m.chat];
-if (chat.isBaneed) return
+handler.all = async function (m) {
+  const chat = global.db.data.chats[m.chat];
+  if (chat?.isBaneed) return;
 
-if (/^@573155227977$/i.test(m.text) ) {
-conn.reply(m.chat, `*No menciones a mí propietario, deja de joder!😡*`, m)
-
-return !0;
+  if (/^@573155227977$/i.test(m.text)) {
+    conn.reply(m.chat, `*No menciones a mi propietario, ¡deja de joder!*`, m);
+    return !0;
+  }
 };
+
 export default handler;
