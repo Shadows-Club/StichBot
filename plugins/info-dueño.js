f314d00e3b66b6cba666d001253c8c0c9306b8f8
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-15 00:54:33 -0500

    Actualizar info-dueño.js

--- plugins/info-dueño.js
+++ plugins/info-dueño.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
   // Crear contacto VCARD
   let list = [{
     displayName: "Jota",
-    vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Jota\nitem1.TEL;waid=573155227977:573155227977\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET:cristianescobar.vx@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/dev.criss_vx\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Perú;;;;\nitem4.X-ABLabel:País\nEND:VCARD`
+    vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Jota\nitem1.TEL;waid=573155227977:573155227977\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET:team.sunflare@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/nee\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Perú;;;;\nitem4.X-ABLabel:País\nEND:VCARD`
   }];
 
   // Enviar solo el contacto, sin adornos
