12bd24db5aa724e80b89e9e767fabff9577fe0c3
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-20 09:36:10 -0500

    Actualizar _test3.js

--- plugins/_test3.js
+++ plugins/_test3.js
@@ -156,26 +156,3 @@ function convertTimeToSpanish(timeText) {
     .replace(/hour/, 'hora').replace(/hours/, 'horas')
     .replace(/minute/, 'minuto').replace(/minutes/, 'minutos');
 }*/
-
-
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn, usedPrefix, command }) => {
-await m.react('🕓')
-try {
-let res = await fetch('https://api.waifu.pics/sfw/neko')
-if (!res.ok) return 
-let json = await res.json()
-if (!json.url) return 
-await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', listo, m)
-await m.react('✅')
-} catch {
-await m.react('✖️')
-}}
-handler.help = ['neko']
-handler.tags = ['search']
-handler.command = ['neko']
-//handler.limit = 1
-handler.register = true 
-
-export default handler
