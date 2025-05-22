bc9052c8f87415bb787d1c8d8d4f145d4f7f4d76
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-22 14:00:20 -0500

    Actualizar plugins

---
+++ plugins/gc-setwelcome.js
@@ -0,0 +1,17 @@
+let handler = async (m, { conn, text, isROwner, isOwner }) => {
+
+if (text) {
+global.db.data.chats[m.chat].sWelcome = text
+conn.reply(m.chat, '*✅ La bienvenida del grupo ha sido configurada*', m)
+
+} else {
+    conn.reply(m.chat, `*${xgc} ¡Escribe el mensaje de bienvenida!*\n✎ *Puedes usar:*\n\n- *\`@user\`* (Mención al Usuario)\n- *\`@group\`* (Nombre del Grupo)\n- *\`@desc\`* (Descripción del Grupo)\n\n> ${emojis} Los @ son opcionales`, m)
+}
+}
+handler.help = ['setwelcome @user + texto']
+handler.tags = ['group']
+handler.command = ['setwelcome', 'bienvenida'] 
+handler.botAdmin = true
+handler.admin = true
+handler.group = true
+export default handler
