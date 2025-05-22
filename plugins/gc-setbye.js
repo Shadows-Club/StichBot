3961882fa3312e8c04c4792e682b8fe414cf37c5
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-22 16:08:39 -0500

    Actualizar gc-setbye.js

--- plugins/gc-setbye.js
+++ plugins/gc-setbye.js
@@ -2,10 +2,10 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
 
 if (text) {
 global.db.data.chats[m.chat].sBye = text
-conn.reply(m.chat, `*✅ La Despedida del grupo ha sido configurada*`, m)  
+conn.reply(m.chat, `*✅ La Despedida del grupo ha sido configurada*`, m, rcanal)  
 
 } else {
-    conn.reply(m.chat, `*${xgc} ¡Escribe el mensaje de despedida!*\n✎ *Puedes usar:*\n\n- *\`@user\`* (Mención al Usuario)\n- *\`@group\`* (Nombre del Grupo)\n- *\`@desc\`* (Descripción del Grupo)\n\n> ${emojis} Los @ son opcionales`, m)
+    conn.reply(m.chat, `*${emojis} ¡Escribe el mensaje de despedida!*\n✎ *Puedes usar:*\n\n- *\`@user\`* (Mención al Usuario)\n- *\`@group\`* (Nombre del Grupo)\n- *\`@desc\`* (Descripción del Grupo)\n\n> ${emojis} Los @ son opcionales`, m, rcanal)
 }
 }
 
