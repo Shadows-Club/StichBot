e9e847b0337913931f23dbff4d4b451dd62f47a7
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-22 16:11:06 -0500

    Actualizar gc-setkick.js

--- plugins/gc-setkick.js
+++ plugins/gc-setkick.js
@@ -5,7 +5,7 @@ global.db.data.chats[m.chat].sKick = text
 conn.reply(m.chat, '*✅ La eliminación del grupo ha sido configurada*', m, rcanal)
 
 } else {
-    conn.reply(m.chat, `*${xgc} ¡Escribe el mensaje de eliminación!*\n✎ *Puedes usar:*\n\n- *\`@user\`* (Mención al Usuario)\n- *\`@group\`* (Nombre del Grupo)\n- *\`@desc\`* (Descripción del Grupo)\n\n> ${emojis} Los @ son opcionales`, m, rcanal)
+    conn.reply(m.chat, `*${emojis} ¡Escribe el mensaje de eliminación!*\n✎ *Puedes usar:*\n\n- *\`@user\`* (Mención al Usuario)\n- *\`@group\`* (Nombre del Grupo)\n- *\`@desc\`* (Descripción del Grupo)\n\n> ${emojis} Los @ son opcionales`, m, rcanal)
 }
 }
 handler.help = ['setkick @user + texto']
