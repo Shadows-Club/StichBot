a30d58aba9bb820b8e110fb9bd6227bd8ba669ad
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-22 10:31:19 -0500

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -1,4 +1,4 @@
-var handler = async (m, { conn, args }) => {
+/*var handler = async (m, { conn, args }) => {
 
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
@@ -12,4 +12,58 @@ handler.command = ['link','linkgroup']
 handler.group = true
 handler.botAdmin = true
 
-export default handler
+export default handler*/
+
+const { generateWAMessageFromContent } = (await import('@whiskeysockets/baileys')).default;
+const { randomBytes } = await import("crypto");
+
+var handler = async (m, { conn }) => {
+    try {
+        let group = m.chat;
+        let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group);
+
+        const messageContent = {
+            interactiveMessage: {
+                body: { text: 'Aquí tienes el enlace del grupo:' },
+                footer: { text: 'Presiona el botón para copiar el enlace' },
+                header: {
+                    title: 'Enlace de Grupo',
+                    hasMediaAttachment: false
+                },
+                nativeFlowMessage: {
+                    buttons: [
+                        {
+                            buttonParamsJson: JSON.stringify({
+                                display_text: 'Copiar enlace',
+                                id: 'copiar_enlace',
+                                copy_code: link
+                            }),
+                            name: "cta_copy"
+                        }
+                    ],
+                    messageParamsJson: "{}",
+                    messageVersion: 1
+                }
+            },
+            messageContextInfo: {
+                messageSecret: randomBytes(32)
+            }
+        };
+
+        const msg = generateWAMessageFromContent(m.chat, messageContent, { userJid: conn.user.id });
+        await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
+
+    } catch (e) {
+        m.reply('Error al generar el enlace del grupo.');
+        console.error(e);
+    }
+};
+
+handler.help = ['link'];
+handler.tags = ['gc'];
+handler.command = ['link', 'linkgroup'];
+
+handler.group = true;
+handler.botAdmin = true;
+
+export default handler;
