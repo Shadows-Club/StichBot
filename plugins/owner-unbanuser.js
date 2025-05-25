34ae35ed09b2e6fbd9ea88f6c1e2d457d699e2d9
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-25 09:35:14 -0500

    Actualizar owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -6,7 +6,7 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
     } else if (args.length >= 1) {
         user = args[0].replace('@', '') + '@s.whatsapp.net';
     } else {
-        await conn.reply(m.chat, `${emojis} Etiqueta o responde al mensaje del usuario que quieras desbanear`, m);
+        await conn.reply(m.chat, `${emoji} Por favor, etiqueta o coloca el número del usuario que quieres desbanear del Bot.`, m);
         return;
     }
     if (db[user]) {
@@ -14,15 +14,15 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
         db[user].banRazon = '';
         const nametag = await conn.getName(user);
         const nn = conn.getName(m.sender);
-        await conn.reply(m.chat, `*✅️ El usuario ${nametag} ha sido desbaneado.*`, m, { mentionedJid: [user] });
-        conn.reply('51927238856@s.whatsapp.net', `*${emojis} El usuario \`${nametag}\` ha sido desbaneado por \`${nn}\``, m, rcanal, );
+        await conn.reply(m.chat, `${done} El usuario *${nametag}* ha sido desbaneado.`, m, { mentionedJid: [user] });
+        conn.reply(`${suittag}@s.whatsapp.net`, `${emoji} El usuario *${nametag}* ha sido desbaneado por *${nn}*.`, m);
     } else {
-        await conn.reply(m.chat, `*⚠️ El usuario no está registrado.*`, m);
+        await conn.reply(m.chat, `${emoji4} El usuario no está registrado.`, m);
     }
 };
-handler.help = ['unbanuser *<@tag>*'];
+handler.help = ['unbanuser <@tag>'];
 handler.command = ['unbanuser'];
-handler.tags = ['owner'];
-handler.prems = true;
-handler.group = true;
+handler.tags = ['mods'];
+handler.rowner = true;
+
 export default handler;
