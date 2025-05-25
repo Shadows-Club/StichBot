121ccb6df816a02bde32275d243fae034e41027b
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-25 09:35:55 -0500

    Actualizar owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -22,7 +22,7 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
 };
 handler.help = ['unbanuser <@tag>'];
 handler.command = ['unbanuser'];
-handler.tags = ['mods'];
+//handler.tags = ['mods'];
 handler.rowner = true;
 
 export default handler;
