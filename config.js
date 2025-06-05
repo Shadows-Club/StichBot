b37ecad9c6f3c6530ba3903e6de64366e753eac6
Shadow's Club 🌹 <shadows.club.vx@gmail.com>
2025-06-05 10:52:52 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-   ['573155227977', 'Jota', true],
+   ['56964656014', 'Igacio', true],
    ['51927238856', 'Cristian Escobar', true],
    ['51990841568', 'Moderador', false],
    ['51965911060', 'Moderador', false]
@@ -22,28 +22,28 @@ global.mods = ['51990841568', '51965911060', '51906168999']
 global.prems = ['51965911060', '51906168999']
 
 
-global.packname = 'Jota Bot MD'
-global.botname = 'Jota Bot - MD'
-global.wm = 'Jota Bot - MD'
+global.packname = 'Garu Bot - MD'
+global.botname = 'Garu Bot - MD'
+global.wm = 'Garu Bot - MD'
 global.author = '𝖲𝗁⍺𝖽ᦅ𝗐′𝗌 𝖢𝗅𝗎𝖻'
 global.dev = '𝖯𑄜𝗐𝖾𝗋𝖾𝖽 𝖻𝗒 𝖲𝗁⍺𝖽ᦅ𝗐′𝗌 𝖢𝗅𝗎𝖻'
 global.errorm = 'Error: ${error.message}'
-global.nombrebot = 'Jota Bot - MD'
-global.textbot = `JOTA BOT MD`
+global.nombrebot = 'Garu Bot - MD'
+global.textbot = `GARU BOT MD`
 global.vs = '1.0.0'
 
 
 global.catalogo = fs.readFileSync('./media/catalogo.jpg')
 
 
-global.repobot = 'https://github.com/CrxstianEscobar/MorchiBot'
+global.repobot = 'https://github.com/Shadows-Club/GaruBot'
 global.grupo = 'https://chat.whatsapp.com/FCS6htvAmlT7nq006lxU4I'
 global.comu = 'https://chat.whatsapp.com/Kn1pPVAO08pFRo7qJnKuh6'
 global.channel = 'https://whatsapp.com/channel/0029VauTE8AHltY1muYir31n'
-global.insta = 'https://www.instagram.com/morchi_yara'
+global.insta = 'https://www.instagram.com/dev.criss_vx'
 
 
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "543876577197-120363317332020195@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'MOONFORCE 乂 TEAM', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "543876577197-120363317332020195@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'Shadow's 乂 Club', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 
 
 global.cheerio = cheerio
