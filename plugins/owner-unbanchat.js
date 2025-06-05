1defda86e2b6f2fc554377b285d3dbd74b446362
Shadow's Club 🌹 <shadows.club.vx@gmail.com>
2025-06-05 15:06:00 -0500

    Actualizar owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -7,7 +7,7 @@ await conn.reply(m.chat, `*${emojis} Chat desbaneado.*`, m)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
-handler.command = ['unbanchat','desbanearchat','desbanchat']
+handler.command = ['unbanchat','desbanearchat','desbanchat', 'garuon']
 handler.rowner = true
 handler.group = true
 
