2aecd62ad059a36d5c49692ca1dafb584744441c
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-06-25 12:46:01 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -4,27 +4,33 @@ import fetch from 'node-fetch'
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return true
 
-  const chat = global.db.data.chats[m.chat]
-  const jid = m.messageStubParameters?.[0]
-  if (!chat?.bienvenida || !jid) return
+  let who = m.messageStubParameters[0]
+  let taguser = `@${who.split('@')[0]}`
+  let chat = global.db.data.chats[m.chat]
+  let defaultImage = 'https://files.catbox.moe/8bywlp.jpg';
 
-  let pp = 'https://files.catbox.moe/hmyrqs.jpg'
-  try {
-    pp = await conn.profilePictureUrl(jid, 'image')
-  } catch {}
-  const img = await (await fetch(pp)).buffer()
+  if (chat.welcome) {
+    let img;
+    try {
+      let pp = await conn.profilePictureUrl(who, 'image');
+      img = await (await fetch(pp)).buffer();
+    } catch {
+      img = await (await fetch(defaultImage)).buffer();
+    }
 
-  const userTag = '@' + jid.split('@')[0]
-  const group = groupMetadata?.subject || 'este grupo'
+  const welcomeMessage = global.db.data.chats[m.chat]?.welcomeMessage || 'Bienvenido/a :';
 
-  const bienvenida = `┌─★ *${global.botname}* \n│「 Bienvenido 」\n└┬★ 「 ${userTag} 」\n   │✑  Bienvenido a\n   │✑  ${group}\n   └───────────────┈ ⳹\n\n${global.club}`
-  const bye = `┌─★ *${global.botname}* \n│「 ADIOS 👋 」\n└┬★ 「 ${userTag} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
+    let bienvenida = `┌─★ *${botname}*\n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑ ${welcomeMessage}\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹\n\n> ${club}`
+      await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] }, { quoted: estilo })
+    } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
 
-  if (m.messageStubType === WAMessageStubType.ADD) {
-    await conn.sendLuffy(m.chat, global.botname, global.textbot, bienvenida, img, img, global.canal, global.estilo)
+const despMessage = global.db.data.chats[m.chat]?.despMessage || 'Se Fue😹';
+
+     let bye = `┌─★ *${botname}*\n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑ ${despMessage}\n   │✑ Ojalá lo violen xD\n   └───────────────┈ ⳹\n\n> ${club}`
+      await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] }, { quoted: estilo })
+    }
   }
 
-  if ([WAMessageStubType.REMOVE, WAMessageStubType.LEAVE].includes(m.messageStubType)) {
-    await conn.sendLuffy(m.chat, global.botname, global.textbot, bye, img, img, global.canal, global.estilo)
-  }
+  return true
 }
