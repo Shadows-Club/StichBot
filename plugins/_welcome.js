83c28237c7834fae51ee0b1a8191bf9780d0c93b
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-06-25 12:35:07 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -15,10 +15,10 @@ await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal, estilo)
 
   if (chat.bienvenida && m.messageStubType == 28) {
     let bye = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹\n\n${club}`
-await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal, estilo)
+await conn.sendLuffy(m.chat, botname, textbot, bye, img, img, canal, estilo)
   }
 
   if (chat.bienvenida && m.messageStubType == 32) {
     let kick = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
-await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal, estilo)
+await conn.sendLuffy(m.chat, botname, textbot, kick, img, img, canal, estilo)
 }}
