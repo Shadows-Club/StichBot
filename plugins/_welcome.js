cab33bda715f631ce85dc6b0c8720bbf54725c84
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-06-25 12:23:57 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -4,107 +4,33 @@ import fetch from 'node-fetch'
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return true
 
-  let groupSize = participants.length
-  if (m.messageStubType == 27) {
-    groupSize++;
-  } else if (m.messageStubType == 28 || m.messageStubType == 32) {
-    groupSize--;
-  }
-  let insta = `https://instagram.com/dev.criss_vx`
   let who = m.messageStubParameters[0]
   let taguser = `@${who.split('@')[0]}`
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
-  let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
-  let txt = `¡Bienvenid@! ${await conn.getName(who)}\nAhora somos ${groupSize} miembros en el grupo.`
-  let txt1 = `¡Adiós! ${await conn.getName(who)}\nAhora somos ${groupSize} miembros en el grupo`
-  let txt2 = `Se salió ${await conn.getName(who)}\nAhora somos ${groupSize} miembros en el grupo.`
-  let sunflare = `Credits to Sunflare`
-  let sunflare1 = `Credits to NovaSpark`
-  let sunflare2 = `Credits to Dev.Criss`
+  let defaultImage = 'https://files.catbox.moe/8bywlp.jpg';
 
-if (chat.welcome && m.messageStubType == 27) {
-  const groupName = groupMetadata.subject
-  const groupDesc = groupMetadata.desc || 'sin descripción'
+  if (chat.welcome) {
+    let img;
+    try {
+      let pp = await conn.profilePictureUrl(who, 'image');
+      img = await (await fetch(pp)).buffer();
+    } catch {
+      img = await (await fetch(defaultImage)).buffer();
+    }
 
-  const msgsWelcome = [
-    `┏─────────────────┐
-「 ${taguser} 」
-𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙓 😊
-𝑫𝒊𝒔𝒇𝒓𝒖𝒕𝒂 𝒅𝒆 𝒕𝒖 𝒆𝒔𝒕𝒂𝒅𝒊𝒂 𝒓𝒆𝒄𝒖𝒆𝒓𝒅𝒂 𝒄𝒖𝒎𝒑𝒍𝒊𝒓 𝒍𝒂𝒔 𝒓𝒆𝒈𝒍𝒂𝒔 𝒚 𝒍𝒆𝒆𝒓 l𝒂 𝒅𝒆𝒔𝒄𝒓𝒊𝒑𝒄𝒊𝒐𝒏 ✨
+  const welcomeMessage = global.db.data.chats[m.chat]?.welcomeMessage || 'Bienvenido/a :';
 
-└───── 𝙅𝙊𝙏𝘼 𝘽𝙊𝙏🐼─────┘`,
-    `┏━━━━━━━━━━━━
-┃──〘 *𝗕𝗜𝗘𝗡𝗩𝗘𝗡𝗜𝗗𝗫* 〙───
-┃━━━━━━━━━━━━
-┃ *_🐼 𝗘𝗡𝗧𝗥𝗢   ${taguser}_* 
-┃ *_Un gusto tenerte aqui_*
-┃ *_Disfruta tu estadía 😇_*
-┗━━━𝙅𝙊𝙏𝘼 𝘽𝙊𝙏━━━━`
-  ]
+    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
+    let bienvenida = `┌─★ *${botname}*\n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑ ${welcomeMessage}\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹\n\n> ${dev}`
+      await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] }, { quoted: estilo })
+    } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
 
+const despMessage = global.db.data.chats[m.chat]?.despMessage || 'Se Fue😹';
 
-  let bienvenida = chat.sWelcome
-    ? chat.sWelcome
-        .replace(/@user/g, taguser)
-        .replace(/@group/g, groupName)
-        .replace(/@desc/g, groupDesc)
-    : msgsWelcome[Math.floor(Math.random() * msgsWelcome.length)]
-
-  await conn.sendLuffy(m.chat, txt, sunflare, bienvenida, img, img, channel, estilo)
-}
-
-if (chat.welcome && m.messageStubType == 28) {
-  const groupName = groupMetadata.subject
-  const groupDesc = groupMetadata.desc || 'sin descripción'
-
-  let ban = chat.sKick
-    ? chat.sKick
-        .replace(/@user/g, taguser)
-        .replace(/@group/g, groupName)
-        .replace(/@desc/g, groupDesc)
-    : `┌─★ 𝙅𝙊𝙏𝘼 𝘽𝙊𝙏🐼 
-│「 ADIOS 👋 」
-└┬★ 「 ${taguser} 」
-   │☠️ *Acabas de ser escupido por puta planta*
-   │💫 *Ni modo, hasta luego...*
-   └────────┈ ⳹`    
-    await conn.sendLuffy(m.chat, txt1, sunflare1, ban, img, img, insta, estilo)
+     let bye = `┌─★ *${botname}*\n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑ ${despMessage}\n   │✑ Ojalá lo violen xD\n   └───────────────┈ ⳹\n\n> ${dev}`
+      await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] }, { quoted: estilo })
+    }
   }
 
-if (chat.welcome && m.messageStubType == 32) {
-  const groupName = groupMetadata.subject
-  const groupDesc = groupMetadata.desc || 'sin descripción'
-
-  const msgsBye = [
-    `*╭┈┈┈┈┈┈┈┈┈┈┈┈┈≫*
-*┊* *${taguser}*
-*┊𝗧𝗨 𝗔𝗨𝗦𝗘𝗡𝗖𝗜𝗔 𝗙𝗨𝗘 𝗖𝗢𝗠𝗢 𝗨𝗡 𝗤𝗟𝗢,* 
-*┊𝗖𝗢𝗡 𝗢𝗟𝗢𝗥 𝗔 𝗠𝗥𝗗!!* 👿
-*╰┈┈┈┈┈┈┈┈┈┈┈┈┈≫*`,
-    `*╭┈┈┈┈┈┈┈┈┈┈┈┈┈≫*
-*┊* *${taguser}*
-*┊𝗔𝗟𝗚𝗨𝗜𝗘𝗡 𝗠𝗘𝗡𝗢𝗦, 𝗤𝗨𝗜𝗘𝗡 𝗧𝗘 𝗥𝗘𝗖𝗨𝗘𝗥𝗗𝗘* 
-*┊𝗦𝗘𝗥𝗔 𝗣𝗢𝗥 𝗟𝗔𝗦𝗧𝗜𝗠𝗔, 𝗔𝗗𝗜𝗢𝗦!!* 👿
-*╰┈┈┈┈┈┈┈┈┈┈┈┈┈≫*`,
-    `*╭┈┈┈┈┈┈┈┈┈┈┈┈┈≫*
-*┊* *${taguser}*
-*┊𝗧𝗨 𝗗𝗘𝗦𝗣𝗘𝗗𝗜𝗗𝗔 𝗡𝗢𝗦 𝗛𝗔𝗥𝗔 𝗟𝗟𝗢𝗥𝗔𝗥,* 
-*┊𝗗𝗘 𝗟𝗔 𝗩𝗘𝗥𝗚𝗨𝗘𝗡𝗭𝗔 𝗤𝗨𝗘 𝗗𝗔𝗕𝗔𝗦!!* 👿
-*╰┈┈┈┈┈┈┈┈┈┈┈┈┈≫*`,
-    `*╭┈┈┈┈┈┈┈┈┈┈┈┈┈≫*
-*┊* *${taguser}*
-*┊𝗗𝗘𝗝𝗢 𝗗𝗘 𝗢𝗟𝗘𝗥 𝗔 𝗠𝗥𝗗,* 
-*┊𝗛𝗔𝗦𝗧𝗔 𝗤𝗨𝗘 𝗧𝗘 𝗟𝗔𝗥𝗚𝗔𝗦𝗧𝗘!!* 👿
-*╰┈┈┈┈┈┈┈┈┈┈┈┈┈≫*`
-  ]
-
-  let bye = chat.sBye
-    ? chat.sBye
-        .replace(/@user/g, taguser)
-        .replace(/@group/g, groupName)
-        .replace(/@desc/g, groupDesc)
-    : msgsBye[Math.floor(Math.random() * msgsBye.length)]
-
-    await conn.sendLuffy(m.chat, txt1, sunflare2, bye, img, img, grupo, estilo)
-  }}
+  return true
+}
